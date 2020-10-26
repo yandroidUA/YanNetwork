4827489ed2cb140657b4cb38446c713d86ac3045
yandroidUA <yanadroidUA@gmail.com>
2020-10-26 23:41:24 +0200

    add place with versions and plugin management

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,9 @@
 rootProject.name = "YanNetwork"
 include("network-ui")
+
+pluginManagement {
+    plugins {
+        val kotlinVersion: String by settings
+        kotlin("jvm") version kotlinVersion
+    }
+}
