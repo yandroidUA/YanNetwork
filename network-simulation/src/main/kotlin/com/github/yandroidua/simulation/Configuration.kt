7abaeca41c16d659849592200e98c6f2f49cd26c
Andrii Yanechko <yanechko.andy@gmail.com>
2020-12-07 23:57:10 +0200

    add results screen

--- network-simulation/src/main/kotlin/com/github/yandroidua/simulation/Configuration.kt
+++ network-simulation/src/main/kotlin/com/github/yandroidua/simulation/Configuration.kt
@@ -13,14 +13,5 @@ data class Configuration(
         var sysPacketSize: Int = 0,
         var size: Int = 0,
         var frameInformationSize: Int = 10,
-        var packetInformationSize: Int = 40,
-        var connectionRnadomDelayRange: IntArray = DEFAULT_DELAY_RANGE,
-        var connectionRandomHalfDuplexAdditionRange: IntArray = DEFAULT_HALF_DUPLEX_DELAY_RANGE,
-        var satteliteRandomDelayRange: IntArray = DEFAULT_SATELLITE_DELAY_RANGE
-) {
-    companion object {
-        val DEFAULT_HALF_DUPLEX_DELAY_RANGE = intArrayOf(2, 7)
-        val DEFAULT_DELAY_RANGE = intArrayOf(0, 5)
-        val DEFAULT_SATELLITE_DELAY_RANGE = intArrayOf(4, 10)
-    }
-}
+        var packetInformationSize: Int = 40
+)
--- network-ui/src/main/kotlin/com/github/yandroidua/ui/screens/SimulationResultScreen.kt
+++ network-ui/src/main/kotlin/com/github/yandroidua/ui/screens/SimulationResultScreen.kt
@@ -1,11 +1,14 @@
 package com.github.yandroidua.ui.screens
 
 import androidx.compose.desktop.AppWindow
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
 import com.github.yandroidua.simulation.models.Mode
+import java.text.SimpleDateFormat
+import java.util.*
 
 fun SimulationResultWindow(
    messageSize: Int,
@@ -17,8 +20,28 @@ fun SimulationResultWindow(
    mode: Mode,
    startTime: Long,
    endTime: Long
-) = AppWindow(size = IntSize(800, 600)).show {
-   Row(modifier = Modifier.fillMaxWidth()) {
-
+) = AppWindow(size = IntSize(800, 600), title = "Results").show {
+   val simpleDateFormat = SimpleDateFormat("dd.MM.YYYY HH:mm:ss.SSS")
+   Column(modifier = Modifier.fillMaxWidth()) {
+      Text(text = "Message size: $messageSize")
+      Spacer(modifier = Modifier.height(height = 4.dp))
+      Text(text = "Frame header size: $frameSystemHeaderSize")
+      Spacer(modifier = Modifier.height(height = 4.dp))
+      Text(text = "Info. package size: $packageInformationSize")
+      Spacer(modifier = Modifier.height(height = 4.dp))
+      Text(text = "Sys. package size: $packageSystemSize")
+      Spacer(modifier = Modifier.height(height = 4.dp))
+      Text(text = "Info. traffic: $informationTraffic")
+      Spacer(modifier = Modifier.height(height = 4.dp))
+      Text(text = "Sys. traffic: $systemTraffic")
+      Spacer(modifier = Modifier.height(height = 4.dp))
+      Text(text = "Start time: $startTime (${simpleDateFormat.format(Date(startTime))})")
+      Spacer(modifier = Modifier.height(height = 4.dp))
+      Text(text = "End time: $endTime (${simpleDateFormat.format(Date(startTime))})")
+      Spacer(modifier = Modifier.height(height = 4.dp))
+      Text(text = "Duration: ${endTime - startTime} millis")
+      Spacer(modifier = Modifier.height(height = 4.dp))
+      Text(text = "Mode: ${mode.name}")
+      Spacer(modifier = Modifier.height(height = 4.dp))
    }
 }
